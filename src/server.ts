ae2ab10f1bc170890e997543655317b0d7920e62
Ricardo <ricardinhogiasson16@gmail.com>
2021-08-24 11:33:10 -0300

    Mascara criada

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-
 import express from "express";
 import cors from "cors";
-
+import "reflect-metadata";
+import "./Database/connect";
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json({ limit: "10000kb" }));
+app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
-    limit: "10000kb",
-    parameterLimit: 1000000,
   })
 );
 
-
-
-app.listen(3335, () => console.log("Server started at http://localhost:3335"));
+app.listen(3336, () => console.log("Server started at http://localhost:3336"));
